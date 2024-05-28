4ba9bee04128057e6a973375c42a234c30f1a30f
sujanrshr <sujanrshr@gmail.com>
2024-05-29 00:18:44 +1000

    review changes 2

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/EmployerServices/EmployerService.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/EmployerServices/EmployerService.jsx
@@ -5,8 +5,10 @@ import regeneratorRuntime from "regenerator-runtime";
 import 'regenerator-runtime/runtime';
 
 const CloseJobService = (id_to_close) => {
+    if (id_to_close === '' || id_to_close === undefined)
+        return;
     console.log("id to close : " + id_to_close);
-    var cookies = Cookies.get('talentAuthToken');
+    let cookies = Cookies.get('talentAuthToken');
     console.log("cookies : " + cookies);
     const link = 'http://localhost:51689/listing/listing/closeJob';
     const idField = id_to_close;
@@ -24,26 +26,24 @@ const CloseJobService = (id_to_close) => {
             headerField            
         )
         .then((response) => {
-            console.log(response);
+            //console.log(response);
             return true;
         })
         .catch((error) => {
-            console.log(error)
             return false;
         });
 }
 
 const GetEmployerService = async (jobsLink) => {
-    var cookies = Cookies.get('talentAuthToken');
-    let retValue;
-    let retData;
+    let cookies = Cookies.get('talentAuthToken');
+    
     const headerField = {        
         headers: {
             'Authorization': 'Bearer ' + cookies,
             'Content-Type': 'application/json'
         }
     };
-    var ret = await axios
+    let ret = await axios
         .get(
             jobsLink,
             headerField
@@ -61,8 +61,6 @@ const GetEmployerService = async (jobsLink) => {
             console.log(error);
             throw error("Unable to get data");
         });
-    //console.log("retValue: " + retValue);
-    //console.log("retData: " + retData);
     //console.log("ret: " + ret);    
 }
 
